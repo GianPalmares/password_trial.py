1e97aeacfcbe532e1b56ea12041259f88e41a2fc
Gian Palmares <134014320+GianPalmares@users.noreply.github.com>
2023-12-12 13:48:04 +0000

    Add files via upload

--- password_trial.py
+++ password_trial.py
@@ -1,39 +1,56 @@
 def login(user_name, password):
+    with open("users.txt", "r") as file:
+        
+        users_dict = {}
 
-    users_dict = {}
+        # file = open("users.txt", "r")
 
-    file = open("users.txt", "r")
+        for line in file:
 
-    for line in file:
-
-        split_users = line.strip().split(", ")
-        users_dict[split_users[0]] = split_users[1]
+            split_users = line.strip().split(", ")
+            users_dict[split_users[0]] = split_users[1]
 
 
-    if user_name not in users_dict:
-        print("Username not available!")
-        choice = input("\nDo you wish to create an account?(yes/no): ")
+        if user_name not in users_dict:
+            print("Username not available!")
+            choice = input("\nDo you wish to create an account?(yes/no): ")
 
-        if choice == "yes":
-            new_user(user_name, password)
+            if choice == "yes":
+                new_user(user_name, password)
+
+            else:
+                print("Closing program!")
+                exit()
+
+        elif password != users_dict[user_name]:
+            print("Invalid Password!")
 
         else:
-            print("Closing program!")
-            exit()
-
-    elif password != users_dict[user_name]:
-        print("Invalid Password!")
-
-    else:
-        print(f"Welcome {user_name}!")
+            print(f"Welcome {user_name}!")
 
 
 def new_user(user_name, password):
-    with open("users.txt", "a") as file:
+    with open("users.txt", "a+") as file:
+
+        users_dict = {}
+        file.seek(0)
+
+        # file2 = open("users.txt", "r")
+
+        for line in file:
+
+            split_users = line.strip().split(", ")
+            users_dict[split_users[0]] = split_users[1]
 
         while True:
 
             user_name = input("\nPlease enter new username: ")
+
+            if user_name in users_dict.keys():
+
+                print("That username already exists! Please enter a different one.")
+                continue
+
             password = input("\nPlease enter new password: ")
 
             if user_name == password:
@@ -41,6 +58,7 @@ def new_user(user_name, password):
                 continue
 
             else:
+                print(f"Welcome {user_name}! Thank you for creating an account with us.")
                 file.write(f"\n{user_name}, {password}")
                 break
 
